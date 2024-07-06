ab5b35ac5d8cf17527a9eb18bff84458e61a81f9
Dumisani Chambela <chmdum003@myuct.ac.za>
2024-07-06 16:39:25 +0200

    added a method to determine who the winner is when playing the game between player one and 2

--- tictac.java
+++ tictac.java
@@ -1,12 +1,41 @@
 import java.sql.Array;
 import java.util.*;
 public class tictac {
-    public int winner(String board){
-
-        int[] winnerNumbers = {16,20,24,30,34,38,44,48,52};
-        for (int i = 0; i < 8; i++) {
-
-        }
+    public static int winner(String board){
+            int startingNo = 16;
+            int startingNo1 = 16;
+            int startingNo2 = 16;
+            for (int j = 0; j < 8; j++) {
+                if (j<2) {
+                    if (board.charAt(startingNo) == 'X' && board.charAt(startingNo + 4) == 'X' && board.charAt(startingNo + 8) == 'X') {
+                        return 1;
+                    } else if (board.charAt(startingNo) == 'O' && board.charAt(startingNo + 4) == 'O' && board.charAt(startingNo + 8) == 'O') {
+                        return 2;
+                    } else startingNo += 14;
+                }
+                else if (j > 3 && j<6){
+                    if (board.charAt(startingNo1)== 'X' && board.charAt(startingNo1+14)=='X' && board.charAt(startingNo1+28)=='X'){
+                        return 1;
+                    }
+                    else if(board.charAt(startingNo1)== 'O' && board.charAt(startingNo1+14)=='O' && board.charAt(startingNo1+28)=='O'){
+                        return 2;
+                    }
+                    else startingNo1+=4;
+                }
+                else if(j == 6){
+                    if (board.charAt(startingNo2) == 'X' && board.charAt(startingNo2 + 18) == 'X' && board.charAt(startingNo2 + 36) == 'X') {
+                        return 1;
+                    } else if (board.charAt(startingNo2) == 'O' && board.charAt(startingNo2 + 18) == 'O' && board.charAt(startingNo2 + 36) == 'O') {
+                        return 2;
+                    } else startingNo2 += 8;
+                } else if (j == 7) {
+                    if (board.charAt(startingNo2) == 'X' && board.charAt(startingNo2 + 10) == 'X' && board.charAt(startingNo2 + 10) == 'X') {
+                        return 1;
+                    } else if (board.charAt(startingNo2) == 'O' && board.charAt(startingNo2 + 10) == 'O' && board.charAt(startingNo2 + 10) == 'O') {
+                        return 2;
+                    }
+                }
+            }
         return 0;
     }
     public static void main(String[] args) {
@@ -19,15 +48,13 @@ public class tictac {
 
             String board = """
                     _____________
-                    | 1 | 2 | 3 |
-                    | 4 | 5 | 6 |
-                    | 7 | 8 | 9 |
-                    _____________""";
+                    | O | X | X |
+                    | X | O | O |
+                    | X | O | X |
+                    -------------""";
 
-        int[] winnerNumbers = {16,20,24,30,34,38,44,48,52};
-        for(int i:winnerNumbers){
-            System.out.println(i);
-        }
+            System.out.println(winner(board));
+
             //System.out.println("---------");
             System.out.print(board.replace(player1.charAt(0),'X'));
             //System.out.println("---------");
