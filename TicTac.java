aa9a2c57b6614cc27c7d11a25bd1c0b30956fe18
dumisxni <dumisanichambela@outlook.com>
2025-01-01 18:33:05 +0200

    commented out the main method

--- TicTac.java
+++ TicTac.java
@@ -54,61 +54,61 @@ public class TicTac {
         return 0;
     }
 
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        Random comp = new Random();
-        TicTac game = new TicTac();
+    // public static void main(String[] args) {
+    //     Scanner keyboard = new Scanner(System.in);
+    //     Random comp = new Random();
+    //     TicTac game = new TicTac();
 
-        for (int h = 0; h < 3; h++) {
-            game.resetBoard();
-            Set<String> compMovesSet = new HashSet<>();
-            ArrayList<String> occupiedPositions = new ArrayList<>();
-            ArrayList<String> compMoves = new ArrayList<>();
+    //     for (int h = 0; h < 3; h++) {
+    //         game.resetBoard();
+    //         Set<String> compMovesSet = new HashSet<>();
+    //         ArrayList<String> occupiedPositions = new ArrayList<>();
+    //         ArrayList<String> compMoves = new ArrayList<>();
 
-            while (compMovesSet.size() < 9) {
-                String move = (comp.nextInt(9) + 1) + "";
-                if (compMovesSet.add(move)) {
-                    compMoves.add(move);
-                }
-            }
+    //         while (compMovesSet.size() < 9) {
+    //             String move = (comp.nextInt(9) + 1) + "";
+    //             if (compMovesSet.add(move)) {
+    //                 compMoves.add(move);
+    //             }
+    //         }
 
-            int i = 0;
-            while (game.winner() == 0 && i < 9 && occupiedPositions.size() < 9) {
-                game.updateBoard(compMoves.get(i).charAt(0), 'X');
-                System.out.println(game.board);
-                occupiedPositions.add(compMoves.get(i));
+    //         int i = 0;
+    //         while (game.winner() == 0 && i < 9 && occupiedPositions.size() < 9) {
+    //             game.updateBoard(compMoves.get(i).charAt(0), 'X');
+    //             System.out.println(game.board);
+    //             occupiedPositions.add(compMoves.get(i));
 
-                if (game.winner() == 1) {
-                    System.out.println("Computer wins");
-                    break;
-                }
+    //             if (game.winner() == 1) {
+    //                 System.out.println("Computer wins");
+    //                 break;
+    //             }
 
-                boolean validMove = true;
-                while (validMove) {
-                    System.out.println("Your turn, choose position on the board");
-                    String playerMove = keyboard.nextLine();
-                    if (!occupiedPositions.contains(playerMove) && playerMove.length() == 1 && Character.isDigit(playerMove.charAt(0))) {
-                        game.updateBoard(playerMove.charAt(0), 'O');
-                        System.out.println(game.board);
-                        occupiedPositions.add(playerMove);
-                        compMoves.remove(playerMove);
-                        validMove = false;
-                    } else {
-                        System.out.println("Enter a valid position");
-                    }
-                }
+    //             boolean validMove = true;
+    //             while (validMove) {
+    //                 System.out.println("Your turn, choose position on the board");
+    //                 String playerMove = keyboard.nextLine();
+    //                 if (!occupiedPositions.contains(playerMove) && playerMove.length() == 1 && Character.isDigit(playerMove.charAt(0))) {
+    //                     game.updateBoard(playerMove.charAt(0), 'O');
+    //                     System.out.println(game.board);
+    //                     occupiedPositions.add(playerMove);
+    //                     compMoves.remove(playerMove);
+    //                     validMove = false;
+    //                 } else {
+    //                     System.out.println("Enter a valid position");
+    //                 }
+    //             }
 
-                if (game.winner() == 2) {
-                    System.out.println("You win");
-                    break;
-                }
+    //             if (game.winner() == 2) {
+    //                 System.out.println("You win");
+    //                 break;
+    //             }
 
-                i++;
-            }
+    //             i++;
+    //         }
 
-            if (game.winner() == 0) {
-                System.out.println("Nobody won");
-            }
-        }
-    }
+    //         if (game.winner() == 0) {
+    //             System.out.println("Nobody won");
+    //         }
+    //     }
+    // }
 }
